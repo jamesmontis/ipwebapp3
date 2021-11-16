298e3c88d47c47bd8442d136a9084f4a1ca28568
J M <jmontis@gmail.com>
2021-11-16 16:01:25 -0600

    Add project files.

---
+++ ipwebapp3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ipwebapp3.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ipwebapp3.Areas.Identity.IdentityHostingStartup))]
+namespace ipwebapp3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ipwebapp3Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ipwebapp3ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ipwebapp3Context>();
+            });
+        }
+    }
+}
